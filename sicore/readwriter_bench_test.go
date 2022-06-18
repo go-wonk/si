932b8429b7ff76af068f7d911c5d8f05ea9cfca2
“wonkdance” <wonkdance@gmail.com>
2022-06-18 12:50:21 +0900

    add WriteAll, EncodeJson, DecodeJson api

--- sicore/readwriter_bench_test.go
+++ sicore/readwriter_bench_test.go
@@ -1,10 +1,13 @@
 package sicore
 
 import (
+	"bytes"
+	"encoding/json"
 	"os"
 	"testing"
 
 	"github.com/go-wonk/si/siutils"
+	"github.com/go-wonk/si/tests/testmodels"
 )
 
 func testCreateFileToRead(fileName, data string) error {
@@ -32,5 +35,28 @@ func Benchmark_readAll(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := readAll(f, DefaultEofChecker)
 		siutils.AssertNilFailB(b, err)
+	}
+}
+
+func BenchmarkReader_Decode(b *testing.B) {
+	var byt []byte = []byte(`{"id":1,"email_address":"asdf","name":"asdf","borrowed":false,"book_id":23}`)
+
+	buf := bytes.NewBuffer(byt)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		r := GetReader(buf, SetJsonDecoder())
+		var s testmodels.Student
+		r.Decode(&s)
+		putReader(r)
+	}
+}
+
+func BenchmarkReader_Unmarshal(b *testing.B) {
+	var byt []byte = []byte(`{"id":1,"email_address":"asdf","name":"asdf","borrowed":false,"book_id":23}`)
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		var s testmodels.Student
+		json.Unmarshal(byt, &s)
 	}
 }
