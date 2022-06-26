8fc99ac5359bf334ea9cbed82a10297828b4a952
“wonkdance” <wonkdance@gmail.com>
2022-06-26 22:23:00 +0900

    add kafka wrapper

---
+++ sikafka/tests/procuder_test.go
@@ -0,0 +1,23 @@
+package sikafka_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-wonk/si/sikafka"
+	"github.com/go-wonk/si/siutils"
+)
+
+func TestProducer_Produce(t *testing.T) {
+	if !onlinetest {
+		t.Skip("skipping online tests")
+	}
+	producer, err := sikafka.DefaultSyncProducer([]string{"kafkahost:9092"})
+	siutils.AssertNilFail(t, err)
+	defer producer.Close()
+
+	sp := sikafka.NewSyncProducer(producer, "tp-test-15")
+	p, o, err := sp.Produce([]byte("10123"), []byte("asdf"))
+	siutils.AssertNilFail(t, err)
+	fmt.Println(p, o)
+}
