f8738c5c11eaa2691ec991d7c06cc59a5d71f16a
wonkdance-da <wonkdance@gmail.com>
2022-07-06 14:18:28 +0900

    fix

--- example/websocket/server/main.go
+++ example/websocket/server/main.go
@@ -1,9 +1,5 @@
 package main
 
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 import (
 	"flag"
 	"html/template"
